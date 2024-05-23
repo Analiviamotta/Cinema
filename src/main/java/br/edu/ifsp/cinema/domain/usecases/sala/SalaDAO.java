a4374126e39ecac3c482c50fa94d62a9c52b12ad
biankyrou <bianca.carvalio@aluno.ifsp.edu.br>
2024-05-22 23:21:05 -0300

    feat: add DAO utils and SalaDAO

---
+++ src/main/java/br/edu/ifsp/cinema/domain/usecases/sala/SalaDAO.java
@@ -0,0 +1,10 @@
+package br.edu.ifsp.cinema.domain.usecases.sala;
+
+import br.edu.ifsp.cinema.domain.entities.sala.Sala;
+import br.edu.ifsp.cinema.domain.usecases.utils.DAO;
+
+import java.util.Optional;
+
+public interface SalaDAO extends DAO<Sala, Integer> {
+    Optional<Sala> findByNumer(int numero);
+}
---
+++ src/main/java/br/edu/ifsp/cinema/domain/usecases/utils/DAO.java
@@ -0,0 +1,12 @@
+package br.edu.ifsp.cinema.domain.usecases.utils;
+import java.util.List;
+import java.util.Optional;
+
+public interface DAO<T, K> {
+    K create(T entity);
+    Optional<T> findOne(K key);
+    List<T> findAll();
+    boolean update(T entity);
+    boolean deleteByKey(K key);
+    boolean delete(T entity);
+}
