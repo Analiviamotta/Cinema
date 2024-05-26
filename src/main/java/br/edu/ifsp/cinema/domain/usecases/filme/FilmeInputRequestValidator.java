ec49af2f675abf7810f515d2a3db07bb9c579c4b
BBranco97 <131369036+BBranco97@users.noreply.github.com>
2024-05-26 10:50:00 -0300

    feat: implement filme input Request Validator

---
+++ src/main/java/br/edu/ifsp/cinema/domain/usecases/filme/FilmeInputRequestValidator.java
@@ -0,0 +1,32 @@
+package br.edu.ifsp.cinema.domain.usecases.filme;
+
+import br.edu.ifsp.cinema.domain.entities.filme.Filme;
+import br.edu.ifsp.cinema.domain.usecases.utils.Notification;
+import br.edu.ifsp.cinema.domain.usecases.utils.Validator;
+
+import java.util.Collections;
+
+public class FilmeInputRequestValidator extends Validator<Filme> {
+        @Override
+        public Notification validate(Filme filme) {
+            Notification notification = new Notification();
+
+            if(filme == null){
+                notification.addError("Filme is null");
+                return notification;
+            }
+
+            if(nullOrEmpty(Collections.singleton(filme.getGenero()))) {
+                notification.addError("Genero do filme is null or empty");
+            }
+            if(nullOrEmpty(Collections.singleton(filme.getSinopse()))) {
+                notification.addError("Sinopse do filme is null or empty");
+            }
+            if(nullOrEmpty(Collections.singleton(filme.getClassificacaoIndicativa()))) {
+                notification.addError("Classificação indicativa do filme is null or empty");
+            }
+
+            return notification;
+        }
+}
+
