ef1dbbfbff8ecac7bd77a3d225ed10d973c61cb4
BBranco97 <barbarabranco1997@gmail.com>
2024-05-30 10:32:59 -0300

    refactor: add valor total in venda entity

--- src/main/java/br/edu/ifsp/cinema/domain/entities/venda/Venda.java
+++ src/main/java/br/edu/ifsp/cinema/domain/entities/venda/Venda.java
@@ -3,6 +3,7 @@ package br.edu.ifsp.cinema.domain.entities.venda;
 import br.edu.ifsp.cinema.domain.entities.exibicao.Exibicao;
 import br.edu.ifsp.cinema.domain.entities.ingresso.Ingresso;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
@@ -11,6 +12,7 @@ public class Venda {
     private List<Ingresso> ingressoList;
     private Exibicao exibicao;
     private Date data;
+    private BigDecimal valorTotal;
 
 
     public Venda(long id, List<Ingresso> ingressoList, Exibicao exibicao, Date data) {
@@ -62,30 +64,44 @@ public class Venda {
         this.exibicao.setQntIngressosDisponiveis(qntIngressosDisponiveis - qntIngressosVendidos);
     }
 
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(List<Ingresso> ingressos) {
+        BigDecimal total = BigDecimal.ZERO;
+
+        for (Ingresso ingresso : ingressos) {
+            BigDecimal precoIngresso = ingresso.getPreco();
+            total = total.add(precoIngresso);
+        }
+
+        this.valorTotal = total;
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
         sb.append("Venda{");
-        sb.append("id = ").append(id);
-        sb.append(", exibicao = ").append(exibicao.toString());
-        sb.append(", data = ").append(data);
+        sb.append("id=").append(id);
+        sb.append(", exibicao=").append(exibicao != null ? exibicao.toString() : "null");
+        sb.append(", data=").append(data);
 
         if (ingressoList != null && !ingressoList.isEmpty()) {
             sb.append(", ingressoList=[");
-
             for (int i = 0; i < ingressoList.size(); i++) {
                 sb.append(ingressoList.get(i).toString());
                 if (i < ingressoList.size() - 1) {
                     sb.append(", ");
                 }
             }
-
             sb.append("]");
         } else {
             sb.append(", ingressoList=null");
         }
 
+        sb.append(", valorTotal=").append(valorTotal);
         sb.append("}");
 
         return sb.toString();
