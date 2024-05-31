7748053b29fd8d3901cb6d56de791df1979ec30e
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-05-31 17:28:24 -0300

    feat: create VendaDAO interface and implements inMemoryVendaDAO

---
+++ src/main/java/br/edu/ifsp/cinema/domain/entities/venda/VendaDAO.java
@@ -0,0 +1,29 @@
+package br.edu.ifsp.cinema.domain.entities.venda;
+import br.edu.ifsp.cinema.domain.entities.ingresso.Ingresso;
+import br.edu.ifsp.cinema.domain.usecases.utils.DAO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface VendaDAO extends DAO<Venda, Long> {
+    @Override
+    Venda create(Venda venda);
+
+    @Override
+    boolean update(Venda venda);
+
+    @Override
+    boolean delete(Venda venda);
+
+    @Override
+    boolean deleteByKey(Long key);
+
+    @Override
+    Optional<Venda> findOne(Long key);
+
+    @Override
+    List<Venda> findAll();
+    @Override
+    boolean isAtivo(Long key);
+    List<Ingresso> listarIngressosDaVenda(Venda venda);
+}
