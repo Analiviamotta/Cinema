fd238319992456a82be494fda65d0bc1261544e6
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-05-31 19:18:48 -0300

    feat: create validator for data of venda

---
+++ src/main/java/br/edu/ifsp/cinema/domain/usecases/venda/VendaInputRequestValidator.java
@@ -0,0 +1,27 @@
+package br.edu.ifsp.cinema.domain.usecases.venda;
+
+import br.edu.ifsp.cinema.domain.entities.venda.Venda;
+import br.edu.ifsp.cinema.domain.usecases.utils.Notification;
+import br.edu.ifsp.cinema.domain.usecases.utils.Validator;
+
+import java.util.Collections;
+
+public class VendaInputRequestValidator extends Validator<Venda>{
+
+    @Override
+    public Notification validate(Venda venda) {
+        Notification notification= new Notification();
+        if(venda == null){
+            notification.addError("venda is null");
+            return notification;
+        }
+        if (venda.getExibicao() == null) {
+            notification.addError("Exibição cannot be null");
+        }
+        if (venda.getIngressoList() == null || venda.getIngressoList().isEmpty()) {
+            notification.addError("ingressos cannot be null or empty");
+        }
+
+        return notification;
+    }
+}
