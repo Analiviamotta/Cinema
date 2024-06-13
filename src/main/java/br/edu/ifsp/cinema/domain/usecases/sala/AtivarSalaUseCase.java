da68cd396202eff36af15b09f7b3c15d3d2c0f5f
biankyrou <bianca.carvalio@aluno.ifsp.edu.br>
2024-06-13 18:41:40 -0300

    adicionando verificacoes adicionais em SalaUseCase

--- src/main/java/br/edu/ifsp/cinema/domain/usecases/sala/AtivarSalaUseCase.java
+++ src/main/java/br/edu/ifsp/cinema/domain/usecases/sala/AtivarSalaUseCase.java
@@ -1,1 +1,27 @@
-// nao pode estar ativo
+package br.edu.ifsp.cinema.domain.usecases.sala;
+
+import br.edu.ifsp.cinema.domain.entities.sala.Sala;
+import br.edu.ifsp.cinema.domain.entities.sala.SalaStatus;
+import br.edu.ifsp.cinema.domain.usecases.filme.FilmeDAO;
+import br.edu.ifsp.cinema.domain.usecases.utils.EntityAlreadyExistsException;
+import br.edu.ifsp.cinema.domain.usecases.utils.EntityNotFoundException;
+
+public class AtivarSalaUseCase {
+    private SalaDAO salaDAO;
+
+    public AtivarSalaUseCase (FilmeDAO filmeDAO) {
+        this.salaDAO = salaDAO;
+    }
+
+    public void ativarFilme(Long id) {
+        Sala sala = salaDAO.findOne(id)
+                .orElseThrow(() -> new EntityNotFoundException("Sala não encontrada"));
+
+        if (sala.getStatus() == SalaStatus.ATIVO) {
+            throw new EntityAlreadyExistsException("A sala já está ativa");
+        }
+
+        sala.setStatus(SalaStatus.ATIVO);
+        salaDAO.update(sala);
+    }
+}
