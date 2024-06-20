efaa2d99fb4fac7a7094e10425fb2a5b71bf0e7b
Poly <polyvstefane@gmail.com>
2024-06-20 03:42:35 -0300

    feat: create database builder

--- src/main/java/br/edu/ifsp/cinema/application/main/Main.java
+++ src/main/java/br/edu/ifsp/cinema/application/main/Main.java
@@ -1,43 +1,24 @@
 package br.edu.ifsp.cinema.application.main;
 
-import br.edu.ifsp.cinema.domain.entities.assento.Assento;
-import br.edu.ifsp.cinema.domain.entities.filme.FilmeGenero;
-import br.edu.ifsp.cinema.domain.entities.sala.Sala;
-import br.edu.ifsp.cinema.domain.usecases.utils.export.PDFExporter;
-import br.edu.ifsp.cinema.domain.usecases.utils.export.Exportable;
-import br.edu.ifsp.cinema.domain.entities.venda.Venda;
-import br.edu.ifsp.cinema.domain.entities.ingresso.Ingresso;
-import br.edu.ifsp.cinema.domain.entities.exibicao.Exibicao;
-import br.edu.ifsp.cinema.domain.entities.filme.Filme;
+import br.edu.ifsp.cinema.HelloApplication;
+import br.edu.ifsp.cinema.application.main.repository.sqlite.util.DatabaseBuilder;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-
-        // Criação de objetos de exemplo para teste
-
-
-
-
-        //String filePath = "relatorio_vendas.pdf";
-        //PDFExporter pdfExporter = new PDFExporter(filePath);
-
-        //pdfExporter.generatesExportableReport(vendas);
-
-        //System.out.println("Relatório de vendas gerado com sucesso em: " + filePath);
-
+        initializeUseCase();
+        createDatabase();
+        HelloApplication.main(args);  // mudar para a view root/home do projeto
     }
 
+    private static void createDatabase(){
+        DatabaseBuilder db = new DatabaseBuilder();
+        db.buildDatabaseIfMissing();
+    }
 
+    private static void initializeUseCase(){
+        //iniciar agora aqui os use cases dps q eu fizer os dao do sqlite e a aninha fizer as telas
 
-
-
-
+    }
 
 }
