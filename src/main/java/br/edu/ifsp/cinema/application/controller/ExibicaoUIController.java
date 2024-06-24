beb7ea0d516ac6ba7213456de44ae63943ce3417
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-06-24 20:14:04 -0300

    feat: Exibicao fxml

--- src/main/java/br/edu/ifsp/cinema/application/controller/ExibicaoUIController.java
+++ src/main/java/br/edu/ifsp/cinema/application/controller/ExibicaoUIController.java
@@ -1,4 +1,14 @@
 package br.edu.ifsp.cinema.application.controller;
 
+import javafx.event.ActionEvent;
+
 public class ExibicaoUIController {
+    public void save(ActionEvent actionEvent) {
+    }
+
+    public void backToPreviousScene(ActionEvent actionEvent) {
+    }
+
+    public void findNumberOfRoom(ActionEvent actionEvent) {
+    }
 }
