109b96716635fe02bc25a06a5cb9272b89ee515d
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-06-23 23:33:05 -0300

    feat: create salaUI

---
+++ src/main/java/br/edu/ifsp/cinema/application/controller/SalaUIfxml.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.cinema.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class SalaUIfxml {
+    public void btnSave(ActionEvent actionEvent) {
+    }
+
+    public void backToPreviousScene(ActionEvent actionEvent) {
+    }
+
+    public void btnOpenAssentosScene(ActionEvent actionEvent) {
+    }
+}
