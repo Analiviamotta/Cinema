2d3ea6b1a2ead78e31a65a1b19194b33fba195dd
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-06-24 20:26:56 -0300

    feat: implements ingressos fxml

--- src/main/java/br/edu/ifsp/cinema/application/controller/IngressoUIController.java
+++ src/main/java/br/edu/ifsp/cinema/application/controller/IngressoUIController.java
@@ -1,4 +1,11 @@
 package br.edu.ifsp.cinema.application.controller;
 
+import javafx.event.ActionEvent;
+
 public class IngressoUIController {
+    public void print(ActionEvent actionEvent) {
+    }
+
+    public void backToPreviousScene(ActionEvent actionEvent) {
+    }
 }
