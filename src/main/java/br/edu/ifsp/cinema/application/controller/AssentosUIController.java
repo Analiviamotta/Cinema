382622dfad001b4bdd206b6afb7faffb55c21e2f
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-06-23 23:51:52 -0300

    feat: create assentosUI and AssentosUIController

---
+++ src/main/java/br/edu/ifsp/cinema/application/controller/AssentosUIController.java
@@ -0,0 +1,11 @@
+package br.edu.ifsp.cinema.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class AssentosUIController {
+    public void backToPreviousScene(ActionEvent actionEvent) {
+    }
+
+    public void deletAssento(ActionEvent actionEvent) {
+    }
+}
