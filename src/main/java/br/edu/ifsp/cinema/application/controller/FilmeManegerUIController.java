e06d2cf96978a8efcb5ec175b46f78792f9575a4
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-06-23 23:04:00 -0300

    feat: initialize controller

--- src/main/java/br/edu/ifsp/cinema/application/controller/FilmeManegerUIController.java
+++ src/main/java/br/edu/ifsp/cinema/application/controller/FilmeManegerUIController.java
@@ -1,4 +1,20 @@
 package br.edu.ifsp.cinema.application.controller;
 
+import javafx.event.ActionEvent;
+
 public class FilmeManegerUIController {
+    public void addFilme(ActionEvent actionEvent) {
+    }
+
+    public void deletFilme(ActionEvent actionEvent) {
+    }
+
+    public void detailFilme(ActionEvent actionEvent) {
+    }
+
+    public void editFilme(ActionEvent actionEvent) {
+    }
+
+    public void backToPreviousScene(ActionEvent actionEvent) {
+    }
 }
