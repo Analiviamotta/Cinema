e66a5c4c658bcf964f416e89973496b6f1e726af
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-06-24 20:54:05 -0300

    feat: implements Exibicao fxml

---
+++ src/main/java/br/edu/ifsp/cinema/application/controller/ExibicaoManagerUIController.java
@@ -0,0 +1,20 @@
+package br.edu.ifsp.cinema.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class ExibicaoManagerUIController {
+    public void backToPreviousScene(ActionEvent actionEvent) {
+    }
+
+    public void addExibicao(ActionEvent actionEvent) {
+    }
+
+    public void edit(ActionEvent actionEvent) {
+    }
+
+    public void inactive(ActionEvent actionEvent) {
+    }
+
+    public void delete(ActionEvent actionEvent) {
+    }
+}
