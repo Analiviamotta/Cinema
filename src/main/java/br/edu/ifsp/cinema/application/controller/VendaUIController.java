958bd15481715a251f57acbf9b205c77019150b7
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-06-24 21:28:26 -0300

    feat: implements vendaUI

---
+++ src/main/java/br/edu/ifsp/cinema/application/controller/VendaUIController.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.cinema.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class VendaUIController {
+    public void backToPreviousScene(ActionEvent actionEvent) {
+    }
+
+    public void addTickets(ActionEvent actionEvent) {
+    }
+
+    public void save(ActionEvent actionEvent) {
+    }
+}
