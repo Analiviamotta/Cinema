ec5aa463b905d5af2f4b93cb049e79f16cf9efff
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-06-24 23:15:04 -0300

    feat: add venda manager fxml and adjustments salaUI fxml

---
+++ src/main/java/br/edu/ifsp/cinema/application/controller/VendaManagerUIController.java
@@ -0,0 +1,20 @@
+package br.edu.ifsp.cinema.application.controller;
+
+import javafx.event.ActionEvent;
+
+public class VendaManagerUIController {
+    public void backToPreviousScene(ActionEvent actionEvent) {
+    }
+
+    public void add(ActionEvent actionEvent) {
+    }
+
+    public void edit(ActionEvent actionEvent) {
+    }
+
+    public void cancel(ActionEvent actionEvent) {
+    }
+
+    public void print(ActionEvent actionEvent) {
+    }
+}
